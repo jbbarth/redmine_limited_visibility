7df95284e04e2670e7daf192d9b74f69f26bcc4c
jbbarth <jeanbaptiste.barth@gmail.com>
2013-10-07 22:55:53 +0200

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_limited_visibility do
+  name 'Redmine Limited Visibility plugin'
+  author 'Jean-Baptiste BARTH'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/jbbarth/redmine_limited_visibility'
+  author_url 'jeanbaptiste.barth@gmail.com'
+end

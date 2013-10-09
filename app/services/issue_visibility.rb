8bf010f16196ba10e4e29397322a9fed91ead331
jbbarth <jeanbaptiste.barth@gmail.com>
2013-10-09 23:01:05 +0200

    Add basic logic for authorizations in IssueVisibility object

--- app/services/issue_visibility.rb
+++ app/services/issue_visibility.rb
@@ -6,8 +6,38 @@ class IssueVisibility
     @issue = issue
   end
 
+  # This method looks at the "authorized_viewers" field on the issue (but it could
+  # be any other type of object, really), and determines if the user can see this
+  # object :
+  #
+  #   authorized_viewers = nil
+  #   authorized_viewers = ""
+  #   authorized_viewers = "*"
+  #   => ANYONE can see
+  #
+  #   authorized_viewers = "||"
+  #   => NOBODY can see
+  #
+  #   authorized_viewers = "|user=23|
+  #   => ONLY User(23) can see
+  #
+  #   authorized_viewers = "|user=23|gorup=34|
+  #   => ONLY User(23) and Group(34) can see
+  #
+  #   authorized_viewers = "|user=23|user=34|organization=52|
+  #   => ONLY User(23), User(34) and Organization(52) can see
+  #
   def authorized?
-    authorizations = issue.authorized_viewers
-    true
+    authorizations = issue.authorized_viewers.to_s
+    # dummy cases, core tests, and existing tickets
+    return true if authorizations == "" || authorizations == "*"
+    # else we tokenize with "|" to get authorizations
+    authorizations_tokens = authorizations.split("|").reject{|t| t == ""}
+    #... we build an array of current users tokens
+    current_user_tokens = ["user=#{user.id}"]
+    current_user_tokens += user.group_ids.map{|gid| "group=#{gid}"}
+    current_user_tokens << "organization=#{user.organization_id}" if user.respond_to?(:organization_id)
+    #... and see if something matches
+    (authorizations_tokens & current_user_tokens).any?
   end
 end
--- spec/fast/issue_visibility_spec.rb
+++ spec/fast/issue_visibility_spec.rb
@@ -2,7 +2,7 @@ require_relative '../fast_spec_helper'
 require_relative '../../app/services/issue_visibility'
 
 describe IssueVisibility do
-  let(:user) { stub :user }
+  let(:user) { stub :user, id: 37, group_ids: [14, 17] }
   let(:issue) { stub :issue }
 
   describe "#new" do
@@ -19,9 +19,45 @@ describe IssueVisibility do
   end
 
   describe "#authorized?" do
-    it "uses Issue#authorized_viewers" do
-      issue.should_receive(:authorized_viewers)
-      IssueVisibility.new(user, issue).authorized?
+    it "passes if authorized_viewers is nil" do
+      issue.stub(:authorized_viewers) { nil }
+      IssueVisibility.new(user, issue).should be_authorized
+    end
+
+    it "passes if authorized_viewers is blank" do
+      issue.stub(:authorized_viewers) { "" }
+      IssueVisibility.new(user, issue).should be_authorized
+    end
+
+    it "passes if authorized_viewers is '*'" do
+      issue.stub(:authorized_viewers) { "*" }
+      IssueVisibility.new(user, issue).should be_authorized
+    end
+
+    it "blocks if authorized_viewers is a list and user is not in the list" do
+      issue.stub(:authorized_viewers) { "||" }
+      IssueVisibility.new(user, issue).should_not be_authorized
+    end
+
+    it "passes if user_id is explicitly in authorizations list" do
+      issue.stub(:authorized_viewers) { "|user=37|" }
+      IssueVisibility.new(user, issue).should be_authorized
+    end
+
+    it "passes if user's organization_id is in authorizations list" do
+      user.stub(:organization_id) { 353 }
+      issue.stub(:authorized_viewers) { "|organization=353|" }
+      IssueVisibility.new(user, issue).should be_authorized
+    end
+
+    it "passes if user has a group in authorizations" do
+      issue.stub(:authorized_viewers) { "|group=17|" }
+      IssueVisibility.new(user, issue).should be_authorized
+    end
+
+    it "blocks if no criteria match" do
+      issue.stub(:authorized_viewers) { "|user=3|group=19|group=45|" }
+      IssueVisibility.new(user, issue).should_not be_authorized
     end
   end
 end

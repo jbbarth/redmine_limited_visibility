7b98507aea93d44d62f612d02a14dff859fb3652
jbbarth <jeanbaptiste.barth@gmail.com>
2013-10-17 18:40:12 +0200

    Don't try to use Rspec.configure#fixture_path= if not available

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,9 @@ require File.expand_path('../fast_spec_helper', __FILE__)
 #fixtures!
 require 'rspec/rails'
 RSpec.configure do |config|
-  config.fixture_path = File.expand_path('../../../../test/fixtures', __FILE__)
+  #if we cannot configure fixtures, it's probably because they are already configured
+  #in an other spec_helper in an other plugin...
+  if config.respond_to?(:fixture_path=)
+    config.fixture_path = File.expand_path('../../../../test/fixtures', __FILE__)
+  end
 end

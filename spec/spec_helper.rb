0f135e7fbfcd4fdd56780f4db4f02be80ad1164c
jbbarth <jeanbaptiste.barth@gmail.com>
2013-10-17 19:32:02 +0200

    Remove fixture config in RSpec since we don't use fixtures for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,3 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path('../../../../config/environment', __FILE__)
 require File.expand_path('../fast_spec_helper', __FILE__)
-
-#fixtures!
-require 'rspec/rails'
-RSpec.configure do |config|
-  #if we cannot configure fixtures, it's probably because they are already configured
-  #in an other spec_helper in an other plugin...
-  if config.respond_to?(:fixture_path=)
-    config.fixture_path = File.expand_path('../../../../test/fixtures', __FILE__)
-  end
-end

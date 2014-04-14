777421f0474f537c10cb51c67f76a933317e0e9a
Vincent Robert <vincent.robert@nanego.com>
2014-04-14 15:09:23 +0200

    Simplify validation patch

--- lib/redmine_limited_visibility/query_patch.rb
+++ lib/redmine_limited_visibility/query_patch.rb
@@ -4,34 +4,12 @@ class Query
 
   alias_method :core_validate_query_filters, :validate_query_filters
 
+  # use standard method to validate filters form,
+  # but ignore "can't be blank" error on authorized_viewers filter because it doesn't require any value
   def validate_query_filters
-    if filters.keys.include?("authorized_viewers")
-      filters.each_key do |field|
-        if values_for(field)
-          case type_for(field)
-            when :integer
-              add_filter_error(field, :invalid) if values_for(field).detect {|v| v.present? && !v.match(/^[+-]?\d+$/) }
-            when :float
-              add_filter_error(field, :invalid) if values_for(field).detect {|v| v.present? && !v.match(/^[+-]?\d+(\.\d*)?$/) }
-            when :date, :date_past
-              case operator_for(field)
-                when "=", ">=", "<=", "><"
-                  add_filter_error(field, :invalid) if values_for(field).detect {|v| v.present? && (!v.match(/^\d{4}-\d{2}-\d{2}$/) || (Date.parse(v) rescue nil).nil?) }
-                when ">t-", "<t-", "t-", ">t+", "<t+", "t+", "><t+", "><t-"
-                  add_filter_error(field, :invalid) if values_for(field).detect {|v| v.present? && !v.match(/^\d+$/) }
-              end
-          end
-        end
-
-        add_filter_error(field, :blank) unless
-            # filter requires one or more values
-            (values_for(field) and !values_for(field).first.blank?) or
-                # filter doesn't require any value
-                ["o", "c", "!*", "*", "t", "ld", "w", "lw", "l2w", "m", "lm", "y", "mine"].include? operator_for(field)
-      end if filters
-    else
-      core_validate_query_filters
-    end
+    core_validate_query_filters
+    m = label_for('authorized_viewers') + " " + l(:blank, :scope => 'activerecord.errors.messages')
+    errors.messages[:base] = errors.messages[:base]-[m] if errors.messages[:base].present? && errors.messages[:base].include?(m)
   end
 
 end

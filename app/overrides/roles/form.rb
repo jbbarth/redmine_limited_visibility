4a03c355f3f88454f70cf4625748aea7060a959a
Vincent Robert <vincent.robert@nanego.com>
2014-04-15 14:08:17 +0200

    Add ability to update roles

--- app/overrides/roles/form.rb
+++ app/overrides/roles/form.rb
@@ -1,18 +1,18 @@
-Deface::Override.new :virtual_path  => "roles/_form",
-                     :name          => "surround_permissions_title",
-                     :surround      => "h3",
-                     :text          => "<div id='permissions_tab_title' class='permissions_tab'> <%= render_original %> </div>"
-Deface::Override.new :virtual_path  => "roles/_form",
-                     :name          => "surround_permissions_form",
-                     :surround      => "#permissions",
-                     :text          => "<div id='permissions_tab_form' class='permissions_tab'> <%= render_original %> </div>"
+Deface::Override.new :virtual_path  => 'roles/_form',
+                     :name          => 'surround_permissions_title',
+                     :surround      => 'h3',
+                     :text          => '<div id="permissions_tab_title" class="permissions_tab"> <%= render_original %> </div>'
+Deface::Override.new :virtual_path  => 'roles/_form',
+                     :name          => 'surround_permissions_form',
+                     :surround      => '#permissions',
+                     :text          => '<div id="permissions_tab_form" class="permissions_tab"> <%= render_original %> </div>'
 
-Deface::Override.new :virtual_path  => "roles/_form",
-                     :name          => "add_checkbox_to_set_limit_visibility_role",
-                     :insert_bottom => ".box.tabular:not([id*=permissions])",
-                     :text          => "<p><%= f.check_box :limit_visibility %></p>"
+Deface::Override.new :virtual_path  => 'roles/_form',
+                     :name          => 'add_checkbox_to_set_limit_visibility_role',
+                     :insert_bottom => '.box.tabular:not([id*=permissions])',
+                     :text          => '<p><%= f.check_box :limit_visibility %></p>'
 
-Deface::Override.new :virtual_path  => "roles/_form",
-                     :name          => "add-role-default-behaviour-regarding-issue-visibility",
-                     :insert_after  => "#permissions_tab_form",
-                     :partial       => "roles/limited_visibility"
+Deface::Override.new :virtual_path  => 'roles/_form',
+                     :name          => 'add-role-default-behaviour-regarding-issue-visibility',
+                     :insert_after  => '#permissions_tab_form',
+                     :partial       => 'roles/limited_visibility'

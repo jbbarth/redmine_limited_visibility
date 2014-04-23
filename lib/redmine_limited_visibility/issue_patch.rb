71898027e2ab72d4eed1baa6c06f771d0302c3c7
Vincent Robert <vincent.robert@nanego.com>
2014-04-23 13:52:50 +0200

    remove unused moethods

--- lib/redmine_limited_visibility/issue_patch.rb
+++ lib/redmine_limited_visibility/issue_patch.rb
@@ -1,15 +1,11 @@
 require_dependency 'issue'
-require_relative '../../app/services/issue_user_visibility'
 
 module RedmineLimitedVisibility
   module IssuePatch
     def self.included(base)
-      base.send(:extend, ClassMethods)
-      base.send(:include, InstanceMethods)
-
       base.class_eval do
         unloadable
-        #alias_method_chain :visible?, :limited_visibility
+
         safe_attributes "authorized_viewers"
 
         alias_method :all_notified_users, :notified_users
@@ -25,11 +21,9 @@ module RedmineLimitedVisibility
 
         def involved_users
           users = []
-          if authorized_viewers.split('|').delete_if(&:blank?)
-            members = Member.joins(:member_roles).where("#{Member.table_name}.project_id = ? AND #{MemberRole.table_name}.role_id IN (?)", project_id, involved_roles_ids)
-            members.each do |m|
-              users << m.user
-            end
+          members = Member.joins(:member_roles).where("#{Member.table_name}.project_id = ? AND #{MemberRole.table_name}.role_id IN (?)", project_id, involved_roles_ids)
+          members.each do |m|
+            users << m.user
           end
           return users
         end
@@ -39,56 +33,10 @@ module RedmineLimitedVisibility
         end
 
         class << self
-          alias_method_chain :visible_condition, :limited_visibility
-
           def involved_roles(authorized_viewers)
             Role.find(authorized_viewers.split('|').delete_if(&:blank?))
           end
         end
-      end
-    end
-
-    module ClassMethods
-      # Issue.visible_condition(user, options={}) is used in cross-issues requests
-      # (either per-project or cross-project). It determines the ability for a user
-      # to access to a list of issues, by generating an SQL fragment that's used
-      # later on in requests on the "issues" table.
-      #
-      # This patch adds a local mask unless the user is admin, in which case he can
-      # "see" all issues naturally. The mask are tested against the dedicated
-      # column "issues.authorized_viewers" which declares who should see an issue
-      # at issue level.
-      #
-      # See the IssueUserVisibility class in app/services for a detailed description
-      # of available masks.
-      def visible_condition_with_limited_visibility(user, options = {})
-        base_condition = visible_condition_without_limited_visibility(user, options)
-        return base_condition if user.admin?
-        conditions = []
-        conditions << "#{Issue.table_name}.authorized_viewers IS NULL"
-        conditions << "#{Issue.table_name}.authorized_viewers IN ('', '*')"
-        conditions << "#{Issue.table_name}.authorized_viewers LIKE '%|user=#{user.id}|%'"
-        conditions << "#{Issue.table_name}.authorized_viewers LIKE '%|organization=#{user.organization_id}|%'" if user.respond_to?(:organization_id) && user.organization_id.present?
-        user.group_ids.each do |gid|
-          conditions << "#{Issue.table_name}.authorized_viewers LIKE '%|group=#{gid}|%'"
-        end
-        user.projects_by_role.each do |role, projects|
-          projects.each do |project|
-            conditions << "#{Issue.table_name}.authorized_viewers LIKE '%|role=#{role.id}/project=#{project.id}|%'"
-          end
-        end
-        limited_condition = "(#{conditions.join(" OR ")})"
-        if base_condition.blank?
-          limited_condition
-        else
-          "(#{base_condition} AND #{limited_condition})"
-        end
-      end
-    end
-
-    module InstanceMethods
-      def visible_with_limited_visibility?(user = nil)
-        visible_without_limited_visibility?(user) && IssueUserVisibility.new(user, self).authorized?
       end
     end
   end
--- spec/models/issue_patch_spec.rb
+++ spec/models/issue_patch_spec.rb
@@ -1,84 +1,8 @@
 require_relative '../spec_helper'
 
 describe RedmineLimitedVisibility::IssuePatch do
+
   let(:issue) { Issue.new }
-  let(:user) { User.new }
-
-  describe "#visible?" do
-    it "patches Issue#visible?" do
-      issue.method(:visible?).should == issue.method(:visible_with_limited_visibility?)
-    end
-
-    it "delegates to original Issue#visible? in normal cases" do
-      issue.should_receive(:visible_without_limited_visibility?).with(user)
-      issue.visible?(user)
-    end
-
-    it "adds additionnal visibility condition" do
-      issue.stub(:visible_without_limited_visibility?).and_return(true)
-      # Redmine specs can be run within other plugins, which can themselves configure
-      # rspec in a form or an other. The fact that redmine comes out of the box with
-      # the "mocha" gem for mocking makes it hard to know at 100% if the mocking framework
-      # will be mocha or rspec at this point. So the "#any_instance" call could be from
-      # mocha or rspec, hence we setup mock expectations on both. Previously we were trying
-      # to detect if mocha is activated but it doesn't appear in RSpec.configuration
-      # directly it seems...
-      # I added some "rescue nil" calls because for whatever fucked reason, "sometimes" I
-      # don't get mocha loaded when running tests only for this plugin, and in that case
-      # it raises an exception I don't ever want to see... Take that haters :)
-      IssueUserVisibility.any_instance.stubs(:authorized?).returns(:result) rescue nil   #mocha mock
-      IssueUserVisibility.any_instance.stub(:authorized?).and_return(:result)            #rspec mock
-      issue.visible?(user).should == :result
-    end
-  end
-
-  describe ".visible_condition" do
-    it "patches Issue.visible_condition" do
-      # actually this:
-      #   Issue.method(:visible_condition).should == Issue.method(:visible_condition_with_limited_visibility)
-      # doesn't work since we use alias_method_chain on a class method and it's a bit... quirky
-      Issue.method(:visible_condition).to_s.should include "visible_condition_with_limited_visibility>"
-    end
-
-    it "allows admins to view everything" do
-      user.stub(:admin?){ true }
-      #ok that's not obvious but it's the base condition, not modified
-      #as we don't work with a real, db-backed user, condition is falsy
-      #and we can't easily stub everything out cause it's class methods...
-      Issue.visible_condition(user).should == Issue.visible_condition_without_limited_visibility(user)
-    end
-
-    it "allows to see issues with no restriction" do
-      Issue.visible_condition(user).should include "issues.authorized_viewers IS NULL OR issues.authorized_viewers IN ('', '*')"
-    end
-
-    it "generates a visible condition based on user_id" do
-      user.stub(:id){ 731 }
-      Issue.visible_condition(user).should include "issues.authorized_viewers LIKE '%|user=731|%'"
-    end
-
-    it "generates a visible condition based on users organization_id if present" do
-      user.stub(:id){ 731 }
-      user.stub(:organization_id){ 36 }
-      Issue.visible_condition(user).should include "issues.authorized_viewers LIKE '%|user=731|%' OR issues.authorized_viewers LIKE '%|organization=36|%'"
-    end
-
-    it "generates a visible condition based on groups if present" do
-      user.stub(:id){ 731 }
-      user.stub(:group_ids){ [5,7] }
-      Issue.visible_condition(user).should include "issues.authorized_viewers LIKE '%|group=5|%' OR issues.authorized_viewers LIKE '%|group=7|%'"
-    end
-
-    it "generates a visible condition based on roles if any" do
-      user.stub(:projects_by_role){
-        { double(:role_1, :id => 1).as_null_object => [double(:project_1, :id => 24), double(:project_2, :id => 27)],
-          double(:role_2, :id => 7).as_null_object => [double(:project_1bis, :id => 24)] }
-      }
-      Issue.visible_condition(user).should include "issues.authorized_viewers LIKE '%|role=1/project=24|%'"
-      Issue.visible_condition(user).should include "issues.authorized_viewers LIKE '%|role=1/project=27|%'"
-      Issue.visible_condition(user).should include "issues.authorized_viewers LIKE '%|role=7/project=24|%'"
-    end
-  end
 
   describe "#authorized_viewers" do
     it "has a authorized_viewers column" do

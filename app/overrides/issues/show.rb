58203ca03b30c7f7404c180e2786277f655a4208
Vincent Robert <vincent.robert@nanego.com>
2014-05-05 15:47:53 +0200

    Remove unused methods

---
+++ app/overrides/issues/show.rb
@@ -0,0 +1,4 @@
+Deface::Override.new :virtual_path => 'issues/show',
+                     :name         => 'show-involved-roles-in-issue-description',
+                     :insert_after => '.attributes',
+                     :partial         => 'issues/show_involved_roles'

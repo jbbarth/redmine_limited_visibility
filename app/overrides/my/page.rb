cade6a9b9e9028a231e19ac261048b920b4c463b
Vincent Robert <vincent.robert@nanego.com>
2014-06-24 12:27:49 +0200

    Possibility to see all issues on My Page

--- app/overrides/my/page.rb
+++ app/overrides/my/page.rb
@@ -3,8 +3,14 @@ Deface::Override.new :virtual_path  => "my/page",
                      :original      => "bc6ae6262eef79aab70c151bfacde1eb8e66512f",
                      :insert_top    => "div.contextual" do
   %(
-    <%= link_to l(:label_my_page_all_roles), new_reminder_path(:back_url => my_page_path),
+    <%= if params[:limited_visibility].present? && params[:limited_visibility] == 'false'
+          link_to l(:label_my_page_my_roles), my_page_path,
                 :class => "icon icon-roles",
-                :id => "show_all_roles" %>
+                :id => "show_all_roles"
+        else
+          link_to l(:label_my_page_all_roles), my_page_path(:limited_visibility => false),
+                :class => "icon icon-roles",
+                :id => "show_all_roles"
+        end %>
   )
 end

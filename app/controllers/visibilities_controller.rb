d081556817aa852cb687f768a99379ab606fa3b9
Vincent Robert <vincent.robert@nanego.com>
2014-07-31 10:39:11 +0200

    Simplify visibilities controller

--- app/controllers/visibilities_controller.rb
+++ app/controllers/visibilities_controller.rb
@@ -1,15 +1,11 @@
 class VisibilitiesController < ApplicationController
-
   # Update 'visibility' roles and do NOT modify 'standard' roles
   # params: member_id (:id) and membership with visibility roles (:role_ids)
   def update_visibility_roles
     @member = Member.find(params[:id]) if params[:id]
     @project = @member.project if @member
-    if params[:membership]
-      # standard roles + updated visibility roles
-      @member.role_ids = @member.role_ids - Role.visibility_roles.pluck(:id)
-      @member.role_ids = @member.role_ids + params[:membership][:role_ids]
-    end
+    # role_ids = standard roles + updated visibility roles
+    @member.role_ids = ((@member.role_ids - Role.visibility_roles.pluck(:id)) + params[:membership][:role_ids]) if params[:membership]
     saved = @member.save
     respond_to do |format|
       format.html { redirect_to_settings_in_projects }
@@ -27,12 +23,9 @@ class VisibilitiesController < ApplicationController
   # Update 'permissions' roles and do NOT modify 'visibility' roles
   def update_permissions_roles_by_organization
     @membership = OrganizationMembership.find(params[:id])
-    if params[:membership]
-      # visibility roles + updated visibility roles
-      @membership.role_ids = @membership.role_ids - Role.permission_roles.pluck(:id)
-      @membership.role_ids = @membership.role_ids + params[:membership][:role_ids]
-    end
-    @membership.save
+    # visibility roles + updated permissions roles
+    @membership.role_ids = ((@membership.role_ids - Role.permission_roles.pluck(:id)) + params[:membership][:role_ids]) if params[:membership]
+    @membership.save!
     # @membership.update_attributes(params[:membership])
     @organization = @membership.organization
     @project = @membership.project
@@ -42,17 +35,12 @@ class VisibilitiesController < ApplicationController
   # Update 'visibility' roles and do NOT modify 'permissions' roles
   def update_visibility_roles_by_organization
     @membership = OrganizationMembership.find(params[:id])
-    if params[:membership]
-      # standard roles + updated visibility roles
-      @membership.role_ids = @membership.role_ids - Role.visibility_roles.pluck(:id)
-      @membership.role_ids = @membership.role_ids + params[:membership][:role_ids]
-    end
-    @membership.save
+    # standard roles + updated visibility roles
+    @membership.role_ids = ((@membership.role_ids - Role.visibility_roles.pluck(:id)) + params[:membership][:role_ids]) if params[:membership]
+    @membership.save!
     # @membership.update_attributes(params[:membership])
     @organization = @membership.organization
     @project = @membership.project
     render 'organization_memberships/update_roles'
   end
-
-
 end

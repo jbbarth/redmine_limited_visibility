56bfaf077fb685d76579f981ed2f015a9cd6d5ac
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-08-06 03:18:11 +0200

    Fix label inversion in specs that I introduced in latest commits

--- spec/helpers/issues_helper_patch_spec.rb
+++ spec/helpers/issues_helper_patch_spec.rb
@@ -10,11 +10,11 @@ describe IssuesHelper do
 
   describe 'show_detail' do
 
-    it 'should display new role with html if old_value is just "||"' do
+    it 'should display new role with html' do
       show_detail(detail_add, false).should include "<strong>Involved members</strong> set to <i>#{contractor_role.name}</i>"
     end
 
-    it 'should display new role with html' do
+    it 'should display new role with html if old_value is just "||"' do
       detail_add.stub(:old_value).and_return("||")
       show_detail(detail_add, false).should include "<strong>Involved members</strong> set to <i>#{contractor_role.name}</i>"
     end

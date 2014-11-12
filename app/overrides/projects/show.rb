fde53fa061a8c4aa046b27584aa6bebb1c6871d6
Vincent Robert <vincent.robert@nanego.com>
2014-11-12 10:51:16 +0100

    Show functional roles on show project page

---
+++ app/overrides/projects/show.rb
@@ -0,0 +1,4 @@
+Deface::Override.new  :virtual_path  => "projects/show",
+                      :name          => "add-member-box-by-function",
+                      :replace       => "erb[loud]:contains('members_box')",
+                      :partial       => "projects/members_box_by_function"

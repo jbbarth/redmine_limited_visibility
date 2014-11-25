0acfff71f6bc6af95103809bd3f2d9470e1b0dc2
Vincent Robert <vincent.robert@nanego.com>
2014-11-25 13:40:02 +0100

    Add some tests to ensure compatibility with the redmine_multiprojects_isue plugin

--- spec/controllers/issues_controller_spec.rb
+++ spec/controllers/issues_controller_spec.rb
@@ -12,11 +12,17 @@ describe IssuesController do
     @request.session[:user_id] = 1
     User.current = User.find(1)
     @project = Project.first
+    @project2 = Project.find(2)
     @membership = Member.new(user_id: User.current.id, project_id: @project.id)
     @membership.roles << Role.first
     @membership.functions << contractor_role
     @membership.save!
+    @membership2 = Member.new(user_id: User.current.id, project_id: @project2.id)
+    @membership2.roles << Role.first
+    @membership2.functions << project_office_role
+    @membership2.save!
     User.current.member_of?(@project).should be true
+    User.current.member_of?(@project2).should be true
   end
 
   it 'adds an "authorized viewers filter" to existing requests' do
@@ -47,5 +53,59 @@ describe IssuesController do
     expect(assigns(:issues)).to_not be_nil
     expect(assigns(:issues)).to include issue1
     expect(assigns(:issues)).to_not include issue2
+
+    # displays all issues when the user has no specific function
+    @membership.functions = []
+    get :index, query_id: q.id
+    expect(assigns(:issues)).to_not be_nil
+    expect(assigns(:issues)).to include issue1
+    expect(assigns(:issues)).to include issue2
+  end
+
+  # Test compatibility with the redmine multiprojects_issue plugin
+  if Redmine::Plugin.installed?(:redmine_multiprojects_issue)
+    describe 'multiprojects_issues' do
+
+      before do
+        @query = IssueQuery.create!(name: "new-query", user: User.current, visibility: 2, project: nil)
+
+        # No authorized_viewers on issues
+        get :index, query_id: @query.id
+        expect(assigns(:issues)).to_not be_nil
+        issues = assigns(:issues).select{ |i| i.project == @project }
+        @issue1 = issues.first
+        @issue2 = issues.second
+        @issue2.projects = [@project2] # issue2 becomes multi-project
+
+        # check if authorized_viewers match user visibility
+        @issue1.update_attribute(:authorized_viewers, "|#{contractor_role.id}|") # User visibility role
+        @issue2.update_attribute(:authorized_viewers, "|#{project_office_role.id}|") # Current user does not match this role
+        @query.filters.merge!({"authorized_viewers" => {:operator=>"mine", :values => [""]}})
+        @query.save!
+      end
+
+      it 'displays issues according to functional roles on secondary projects' do
+        get :index, query_id: @query.id
+        expect(assigns(:issues)).to_not be_nil
+        expect(assigns(:issues)).to include @issue1
+        expect(assigns(:issues)).to include @issue2 # issue2 is now visible because user is member of the secondary project with correct role
+      end
+
+      it 'displays no issue when the user has NOT the specific function' do
+        @membership2.functions = [contractor_role]
+        get :index, query_id: @query.id
+        expect(assigns(:issues)).to_not be_nil
+        expect(assigns(:issues)).to include @issue1
+        expect(assigns(:issues)).to_not include @issue2
+      end
+
+      it 'displays all issues when the user has no specific function' do
+        @membership2.functions = []
+        get :index, query_id: @query.id
+        expect(assigns(:issues)).to_not be_nil
+        expect(assigns(:issues)).to include @issue1
+        expect(assigns(:issues)).to include @issue2
+      end
+    end
   end
 end

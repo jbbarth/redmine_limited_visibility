278c83645fa6524fcc56cb03b876f6d1b5672b1a
Vincent Robert <vincent.robert@nanego.com>
2015-01-19 14:32:15 +0100

    Show functional roles in user memberships screen

--- app/overrides/users/_memberships.rb
+++ app/overrides/users/_memberships.rb
@@ -8,3 +8,14 @@ Deface::Override.new :virtual_path  => "users/_memberships",
                         <label><%= check_box_tag 'membership[function_ids][]', function.id, false, :id => nil %> <%=h function %></label>
                       <% end %></p>
 eos
+
+Deface::Override.new :virtual_path  => "users/_memberships",
+                     :name          => "add-table-title-for-functional-roles",
+                     :insert_before => "th:last",
+                     :original      => '0d5041d1f9ab03cafbc2e51905fdbd13944bfbbe',
+                     :text          => '<th><%= l :label_visibility_roles %></th>'
+Deface::Override.new :virtual_path  => "users/_memberships",
+                     :name          => "add-functional-roles-to-current-memberships",
+                     :original      => '0d5041d1f9ab03cafbc2e51905fdbd13944bfbbe',
+                     :insert_after  => "td.roles",
+                     :partial       => 'users/memberships_functions'

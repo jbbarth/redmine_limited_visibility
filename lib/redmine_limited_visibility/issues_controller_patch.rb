72fef7fee44d85a946ef24b3ac9a797197881156
Vincent Robert <vincent.robert@nanego.com>
2015-02-10 16:49:46 +0100

    Ensure tested param act as a string

--- lib/redmine_limited_visibility/issues_controller_patch.rb
+++ lib/redmine_limited_visibility/issues_controller_patch.rb
@@ -7,7 +7,7 @@ class IssuesController < ApplicationController
   private
     def set_assigned_to_function_id
       if params[:issue].present? && params[:issue][:assigned_to_id].present?
-        if params[:issue][:assigned_to_id].include?("function")
+        if params[:issue][:assigned_to_id].to_s.include?("function")
           params[:issue][:assigned_to_id].slice! "function-"
           params[:issue][:assigned_to_function_id] = params[:issue][:assigned_to_id]
           params[:issue][:assigned_to_id] = nil

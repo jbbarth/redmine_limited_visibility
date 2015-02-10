7159cc1d4dd940ba13221e441f9823a0042ffcb6
Vincent Robert <vincent.robert@nanego.com>
2015-02-10 16:46:51 +0100

    Fix bug if there is no param assigned_to in issue request

--- lib/redmine_limited_visibility/issues_controller_patch.rb
+++ lib/redmine_limited_visibility/issues_controller_patch.rb
@@ -6,7 +6,7 @@ class IssuesController < ApplicationController
 
   private
     def set_assigned_to_function_id
-      if params[:issue][:assigned_to_id].present?
+      if params[:issue].present? && [:assigned_to_id].present?
         if params[:issue][:assigned_to_id].include?("function")
           params[:issue][:assigned_to_id].slice! "function-"
           params[:issue][:assigned_to_function_id] = params[:issue][:assigned_to_id]

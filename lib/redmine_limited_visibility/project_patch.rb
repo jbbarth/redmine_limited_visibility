7c23d7007d17f695d02720ffd25ebe45688ff644
Vincent Robert <vincent.robert@nanego.com>
2015-02-26 17:34:56 +0100

    Patch copy_members method so functional roles are also duplicated for members and organizations

--- lib/redmine_limited_visibility/project_patch.rb
+++ lib/redmine_limited_visibility/project_patch.rb
@@ -5,6 +5,8 @@ class Project
   has_many :project_functions, :dependent => :destroy
   has_many :functions, :through => :project_functions
 
+  has_many :organization_functions
+
   # Builds a nested hash of users sorted by function and organization
   # => { Function(1) => { Org(1) => [ User(1), User(2), ... ] } }
   #
@@ -34,4 +36,54 @@ class Project
       end
     end
   end
+
+  # Copies members from +project+
+  def copy_members(project)
+
+    # Copy users first, then groups to handle members with inherited and given roles
+    members_to_copy = []
+    members_to_copy += project.memberships.select {|m| m.principal.is_a?(User)}
+    members_to_copy += project.memberships.select {|m| !m.principal.is_a?(User)}
+
+    members_to_copy.each do |member|
+      new_member = Member.new
+      new_member.attributes = member.attributes.dup.except("id", "project_id", "created_on")
+      # only copy non inherited roles
+      # inherited roles will be added when copying the group membership
+      role_ids = member.member_roles.reject(&:inherited?).collect(&:role_id)
+      next if role_ids.empty?
+      new_member.role_ids = role_ids
+
+      # TODO: Refactor this patch and use alias_method_chain in order to keep untouched the original 'copy_members' method
+      # Start
+      function_ids = member.member_functions.map(&:function_id)
+      new_member.function_ids = function_ids
+      # End of the patch
+
+      new_member.project = self
+      self.members << new_member
+    end
+
+    # Patch - Restart
+    self.functions = project.project_functions.map(&:function)
+
+    # TODO Only if organization plugin is installed
+    orga_functions_to_copy = project.organization_functions
+    orga_functions_to_copy.each do |orga_function|
+      new_orga_function = OrganizationFunction.new
+      new_orga_function.attributes = orga_function.attributes.dup.except("id", "project_id")
+      self.organization_functions << new_orga_function
+    end
+
+    orga_roles_to_copy = project.organization_roles
+    orga_roles_to_copy.each do |orga_role|
+      new_orga_role = OrganizationRole.new
+      new_orga_role.attributes = orga_role.attributes.dup.except("id", "project_id")
+      self.organization_roles << new_orga_role
+      self.organization_roles << new_orga_role
+    end
+    # End of the patch
+
+  end
+
 end

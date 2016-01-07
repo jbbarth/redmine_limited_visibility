b9885d341d73066354961cad146c1aa5badabd63
Vincent Robert <vincent.robert@nanego.com>
2016-01-07 16:36:56 +0100

    Fix a bug when the given role does not exist anymore

--- lib/redmine_limited_visibility/member_patch.rb
+++ lib/redmine_limited_visibility/member_patch.rb
@@ -12,20 +12,24 @@ class Member < ActiveRecord::Base
   #
   # Example:
   #   Member.create_principal_memberships(user, :project_ids => [2, 5], :role_ids => [1, 3], :function_ids => [5, 6])
+
   def self.create_principal_memberships(principal, attributes)
     members = []
     if attributes
       project_ids = Array.wrap(attributes[:project_ids] || attributes[:project_id])
       role_ids = []
-      role_ids |= attributes[:role_ids] if attributes[:role_ids]
+      role_ids |= attributes[:role_ids].map(&:to_i) if attributes[:role_ids]
       function_ids = []
-      function_ids |= attributes[:function_ids] if attributes[:function_ids]
+      function_ids |= attributes[:function_ids].map(&:to_i) if attributes[:function_ids]
       project_ids.each do |project_id|
         if Redmine::Plugin.installed?(:redmine_organizations)
-          role_ids_by_project = role_ids | OrganizationRole.where(organization_id: principal.organization_id, project_id: project_id).all.collect{|r| r.role_id}
+          # Member must have the selected role AND the role given by his/her organization
+          all_roles_ids = Role.all.collect(&:id)
+          role_ids_by_project = role_ids | OrganizationRole.where(organization_id: principal.organization_id, project_id: project_id).all.map{|r| all_roles_ids.include?(r.role_id) ? r.role_id : nil}
         end
         if Redmine::Plugin.installed?(:redmine_limited_visibility)
-          function_ids_by_project = function_ids | OrganizationFunction.where(organization_id: principal.organization_id, project_id: project_id).all.collect{|f| f.function_id}
+          all_functions_ids = Function.all.collect(&:id)
+          function_ids_by_project = function_ids | OrganizationFunction.where(organization_id: principal.organization_id, project_id: project_id).all.map{|f| all_functions_ids.include?(f.function_id) ? f.function_id : nil}
           project = Project.where('id = ?', project_id).first
           function_ids_by_project.each do |function_id|
             if project.functions.present? && !project.functions.map(&:id).include?(function_id.to_i)
@@ -39,4 +43,5 @@ class Member < ActiveRecord::Base
     end
     members
   end
+
 end

4fc54d5ff87f151fd72f1ac226208e27f224a5ac
Vincent Robert <vincent.robert@nanego.com>
2016-10-18 10:33:01 +0200

    Repeat override when using an issue form with positions

--- app/overrides/issues/form.rb
+++ app/overrides/issues/form.rb
@@ -2,3 +2,7 @@ Deface::Override.new :virtual_path  => 'issues/_form',
                      :name          => 'add-authorized-viewers-in-issues-new',
                      :insert_before => "erb[silent]:contains(\"if @issue.safe_attribute? 'subject'\")",
                      :partial       => 'issues/authorized_viewers_form'
+Deface::Override.new :virtual_path  => 'issues/_form_with_positions',
+                     :name          => 'add-authorized-viewers-in-issues-new',
+                     :insert_before => "erb[silent]:contains(\"if @issue.safe_attribute? 'subject'\")",
+                     :partial       => 'issues/authorized_viewers_form'

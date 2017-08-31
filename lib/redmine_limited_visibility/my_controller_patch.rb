e0599b3714d094b934c6accd49adf1dbddc31269
Vincent Robert <vincent.robert@nanego.com>
2017-08-31 14:12:17 +0200

    Reflect changes in my_page from Redmine 3.4

--- lib/redmine_limited_visibility/my_controller_patch.rb
+++ lib/redmine_limited_visibility/my_controller_patch.rb
@@ -6,7 +6,9 @@ class MyController < ApplicationController
   # Show user's page with limited visibility
   def page
     @user = User.current
+    @groups = @user.pref.my_page_groups
     @blocks = @user.pref[:my_page_layout] || DEFAULT_LAYOUT
     @visibility_condition = IssueQuery.new.sql_conditions_for_functions_per_projects('authorized_viewers') unless params[:limited_visibility] == 'false'
   end
+
 end

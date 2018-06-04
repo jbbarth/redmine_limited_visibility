e7b34bc0eaca788ead0fea04bef8e72ff501da85
Vincent Robert <vincent.robert@nanego.com>
2018-06-04 16:36:23 +0200

    Update according to changes in the Organizations plugin

--- app/overrides/members/_new_form.rb
+++ app/overrides/members/_new_form.rb
@@ -1,7 +1,4 @@
-Deface::Override.new :virtual_path  => 'members/_new_form',
-                     :name          => 'add-functions-to-new-members-form',
-                     :insert_after  => "fieldset:contains('membership[role_ids][]')",
-                     :text       => <<FIELDSET_WITH_FUNCTIONS
+FIELDSET_WITH_FUNCTIONS = <<FIELDSET_WITH_FUNCTIONS
 <fieldset class="box">
 <legend><%= toggle_checkboxes_link('.functions-selection input') %><%= l(:label_functional_roles) %></legend>
   <div class="functions-selection">
@@ -11,3 +8,13 @@ Deface::Override.new :virtual_path  => 'members/_new_form',
   </div>
 </fieldset>
 FIELDSET_WITH_FUNCTIONS
+
+Deface::Override.new :virtual_path  => 'members/_new_form',
+                     :name          => 'add-functions-to-new-members-form',
+                     :insert_after  => "fieldset:contains('membership[role_ids][]')",
+                     :text       => FIELDSET_WITH_FUNCTIONS
+
+Deface::Override.new :virtual_path  => 'organizations/memberships/_new_form',
+                     :name          => 'add-functions-to-new-orga-members-form',
+                     :insert_after  => "fieldset:contains('membership[role_ids][]')",
+                     :text       => FIELDSET_WITH_FUNCTIONS

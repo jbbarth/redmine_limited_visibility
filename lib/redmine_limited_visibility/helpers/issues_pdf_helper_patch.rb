21f0addbe34b2df8c6cd7e452cfb48fadaae2229
Vincent Robert <vincent.robert@nanego.com>
2018-12-14 14:33:24 +0100

    Reflect changes from Redmine 4.0

--- lib/redmine_limited_visibility/helpers/issues_pdf_helper_patch.rb
+++ lib/redmine_limited_visibility/helpers/issues_pdf_helper_patch.rb
@@ -14,23 +14,26 @@ module Redmine
             else
               # core method
               s = if column.is_a?(QueryCustomFieldColumn)
-                  cv = object.visible_custom_field_values.detect {|v| v.custom_field_id == column.custom_field.id}
-                  show_value(cv, false)
+                    cv = object.visible_custom_field_values.detect {|v| v.custom_field_id == column.custom_field.id}
+                    show_value(cv, false)
                   else
-                    if object.class.method_defined? column.name
-                      value = object.send(column.name)
-                      if column.name == :subject
-                        value = "  " * level + value
-                      end
-                      if value.is_a?(Date)
-                        format_date(value)
-                      elsif value.is_a?(Time)
-                        format_time(value)
-                      else
-                        value
-                      end
+                    value = object.send(column.name)
+                    case column.name
+                    when :subject
+                      value = "  " * level + value
+                    when :attachments
+                      value = value.to_a.map {|a| a.filename}.join("\n")
                     end
-                end
+                    if value.is_a?(Date)
+                      format_date(value)
+                    elsif value.is_a?(Time)
+                      format_time(value)
+                    elsif value.is_a?(Float)
+                      sprintf "%.2f", value
+                    else
+                      value
+                    end
+                  end
             end
             s.to_s
           end

03b9a0fc391a04f748d978d84b0d30d07de758b1
Vincent Robert <vincent.robert@nanego.com>
2019-02-13 16:22:12 +0100

    Simplify functions assignment by organization

--- lib/redmine_limited_visibility/controllers/organizations_memberships_controller_patch.rb
+++ lib/redmine_limited_visibility/controllers/organizations_memberships_controller_patch.rb
@@ -2,7 +2,9 @@ require_dependency 'organizations_controller'
 require_dependency 'organizations/memberships_controller'
 
 class Organizations::MembershipsController < ApplicationController
-  def give_new_organization_functions_to_all_members(project:, organization:, organization_functions:, previous_organization_functions:)
+  def give_new_organization_functions_to_all_members(project:, organization:)
+    organization_functions = @organization.organization_functions.where(project_id: @project.id).map(&:function).reject(&:blank?)
+    previous_organization_functions = @organization.default_functions_by_project(@project)
     members = Member.joins(:user).where("project_id = ? AND users.organization_id = ?", project.id, organization.id)
     members.each do |member|
       personal_functions = member.functions - previous_organization_functions

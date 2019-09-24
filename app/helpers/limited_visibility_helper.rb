03f2436df72acd01ac868710a694d0765fffb599
Vincent Robert <vincent.robert@nanego.com>
2019-09-24 12:51:10 +0200

    Fix typo

--- app/helpers/limited_visibility_helper.rb
+++ app/helpers/limited_visibility_helper.rb
@@ -8,7 +8,7 @@ module LimitedVisibilityHelper
       else
         current_functions = ProjectFunctionTracker.joins(:project_function).where("project_id = ? AND tracker_id = ?", issue.project_id, issue.tracker_id)
         if current_functions.present? # current tracker has at least one functional role in settings
-          viewers = current_functions.select{ |f| f.checked == true }.map{ |c| c.function.id }
+          viewers = current_functions.select {|f| f.checked == true}.map {|c| c.function.id}
         else # else check all functions
           viewers = Function.available_functions_for(issue.project).sorted.pluck(:id)
         end
@@ -43,7 +43,7 @@ module LimitedVisibilityHelper
           end
           enabled_functions = enabled_functions & Function.available_functions_for(@project).sorted
           enabled_functions.sort_by {|a| a.position}
-          viewers = enabled_functions.map{ |f| f.id }
+          viewers = enabled_functions.map {|f| f.id}
         else # current user has no visibility role (can see all issues available for the current project)
           viewers = Function.available_functions_for(issue.project).sorted.pluck(:id)
         end
@@ -59,32 +59,34 @@ module LimitedVisibilityHelper
   end
 
   def functional_roles_for_current_user(project)
-    Function.joins(:members).where(:members => { :user_id => User.current.id, :project_id => project.id }).sorted
+    Function.joins(:members).where(:members => {:user_id => User.current.id, :project_id => project.id}).sorted
   end
 
   # Returns a string for users/groups option tags
-  def assignable_options_for_select(issue, users, selected=nil)
+  def assignable_options_for_select(issue, users, selected = nil)
     s = ''
-    if @issue.project.present? && @issue.project.module_enabled?("limited_visibility")
-      functional_roles_ids = Function.available_functions_for(issue.project).sorted.pluck(:id)
-      functional_roles_ids.each do |function_id|
-        s << content_tag('option', "#{Function.find(function_id).name}", :value => "function-#{function_id}", :selected => (option_value_selected?(function_id, selected) || function_id == selected))
+    if @issue.project.present?
+      if @issue.project.module_enabled?("limited_visibility")
+        functional_roles_ids = Function.available_functions_for(issue.project).sorted.pluck(:id)
+        functional_roles_ids.each do |function_id|
+          s << content_tag('option', "#{Function.find(function_id).name}", :value => "function-#{function_id}", :selected => (option_value_selected?(function_id, selected) || function_id == selected))
+        end
+        s << "<option disabled>──────────────</option>"
       end
-      s << "<option disabled>──────────────</option>"
-    end
-    if users.include?(User.current)
-      s << content_tag('option', "<< #{l(:label_me)} >>", :value => User.current.id)
-    end
-    groups = ''
+      if users.include?(User.current)
+        s << content_tag('option', "<< #{l(:label_me)} >>", :value => User.current.id)
+      end
+      groups = ''
 
-    users.sort.each do |element|
-      selected_attribute = ' selected="selected"' if option_value_selected?(element, selected) || element.id.to_s == selected
-      functional_roles_ids = issue.project.functions_per_user[element.id]
-      functional_roles_attribute = functional_roles_ids.present? ? " functional_roles='#{functional_roles_ids.join(',')}'" : ""
-      (element.is_a?(Group) ? groups : s) << %(<option value="#{element.id}"#{selected_attribute}#{functional_roles_attribute}>#{h element.name}</option>)
-    end
-    unless groups.empty?
-      s << %(<optgroup label="#{h(l(:label_group_plural))}">#{groups}</optgroup>)
+      users.sort.each do |element|
+        selected_attribute = ' selected="selected"' if option_value_selected?(element, selected) || element.id.to_s == selected
+        functional_roles_ids = issue.project.functions_per_user[element.id]
+        functional_roles_attribute = functional_roles_ids.present? ? " functional_roles='#{functional_roles_ids.join(',')}'" : ""
+        (element.is_a?(Group) ? groups : s) << %(<option value="#{element.id}"#{selected_attribute}#{functional_roles_attribute}>#{h element.name}</option>)
+      end
+      unless groups.empty?
+        s << %(<optgroup label="#{h(l(:label_group_plural))}">#{groups}</optgroup>)
+      end
     end
     s.html_safe
   end

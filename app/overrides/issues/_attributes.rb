9b28b4274cb8149dfaba637fc7976b176181a5cf
Vincent Robert <vincent.robert@nanego.com>
2019-11-04 12:12:29 +0100

    Improve compatibility with redmine_customize_core_fields plugin

--- app/overrides/issues/_attributes.rb
+++ app/overrides/issues/_attributes.rb
@@ -3,3 +3,9 @@ Deface::Override.new :virtual_path      => 'issues/_attributes',
                      :replace           => "erb[silent]:contains(\"if @issue.safe_attribute? 'assigned_to_id'\")",
                      :closing_selector  => "erb[silent]:contains(\"end\")",
                      :partial           => 'issues/assign_to_select_box'
+
+Deface::Override.new :virtual_path      => 'issues/_form_with_positions',
+                     :name              => 'show-functional-roles-in-assignable-select',
+                     :replace           => "erb[silent]:contains(\"if @issue.safe_attribute? 'assigned_to_id'\")",
+                     :closing_selector  => "erb[silent]:contains(\"end\")",
+                     :partial           => 'issues/assign_to_select_box'

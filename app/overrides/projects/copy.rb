8f9e7bbc084eb21f1932aa97d4baa5b87b179390
Vincent Robert <vincent.robert@nanego.com>
2020-01-14 11:50:58 +0100

    Fix deface override insertion

--- app/overrides/projects/copy.rb
+++ app/overrides/projects/copy.rb
@@ -1,4 +1,6 @@
 Deface::Override.new  :virtual_path  => "projects/copy",
                       :name          => "include-functions-when-copying",
-                      :insert_before => "erb[loud]:contains('hidden_field_tag')",
-                      :partial       => "projects/copy_functions"
+                      :insert_after  => ".block:contains('@source_project.wiki.nil?')",
+                      :text          => <<EOS
+<label class="block"><%= check_box_tag 'only[]', 'functions', true, :id => nil %> <%= l(:label_functional_roles) %> (<%= @source_project.functions.count %>)</label>
+EOS

6cc5cc5e8214e05537782ecf328904ceb4f91c01
Vincent Robert <vincent.robert@nanego.com>
2020-01-16 16:17:03 +0100

    Fix default visibility when author has multiple functions

--- app/helpers/limited_visibility_helper.rb
+++ app/helpers/limited_visibility_helper.rb
@@ -24,26 +24,30 @@ module LimitedVisibilityHelper
   end
 
   def function_ids_for_current_viewers(issue)
-    viewers = []
     if issue.new_record? # create new issue
       if issue.authorized_viewer_ids.present?
         viewers = issue.authorized_viewer_ids
       else
         current_functions = functional_roles_for_current_user(issue.project)
         if current_functions.present? # current user has at least one functional role
-          enabled_functions = []
+          activated_functions = []
           current_functions.each do |f|
             functions_per_project = ProjectFunction.where('project_id = ? AND function_id = ?', issue.project_id, f.id)
+            enabled_functions_per_project = []
             functions_per_project.each do |pf|
               if pf.authorized_viewer_ids.present?
-                enabled_functions |= Function.where("id in (?)", pf.authorized_viewer_ids).sorted
+                enabled_functions_per_project |= Function.where("id in (?)", pf.authorized_viewer_ids).sorted
               end
             end
-            enabled_functions |= Function.where("id in (?)", f.authorized_viewer_ids).sorted if enabled_functions.empty?
+            if enabled_functions_per_project.present?
+              activated_functions |= enabled_functions_per_project
+            else
+              activated_functions |= Function.where("id in (?)", f.authorized_viewer_ids).sorted
+            end
           end
-          enabled_functions = enabled_functions & Function.available_functions_for(@project).sorted
-          enabled_functions.sort_by {|a| a.position}
-          viewers = enabled_functions.map {|f| f.id}
+          activated_functions = activated_functions & Function.available_functions_for(@project).sorted
+          activated_functions.sort_by {|a| a.position}
+          viewers = activated_functions.map {|f| f.id}
         else # current user has no visibility role (can see all issues available for the current project)
           viewers = Function.available_functions_for(issue.project).sorted.pluck(:id)
         end

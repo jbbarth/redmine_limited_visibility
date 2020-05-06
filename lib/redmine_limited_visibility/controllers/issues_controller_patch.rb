510e3a9a7ca2e79d60919c252f4ccdd0b9c2d837
Vincent Robert <vincent.robert@nanego.com>
2020-05-06 16:24:24 +0200

    Fix assign_to_function removal

--- lib/redmine_limited_visibility/controllers/issues_controller_patch.rb
+++ lib/redmine_limited_visibility/controllers/issues_controller_patch.rb
@@ -9,7 +9,7 @@ class IssuesController < ApplicationController
   private
     def set_assigned_to_function_id
       if params[:issue].present?
-        if params[:issue][:assigned_to_id].present?
+        if params[:issue][:assigned_to_id]
           if params[:issue][:assigned_to_id].to_s.include?("function")
             params[:issue][:assigned_to_id].slice! "function-"
             params[:issue][:assigned_to_function_id] = params[:issue][:assigned_to_id]

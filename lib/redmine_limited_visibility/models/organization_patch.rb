063bb77e258ec7093dba47832a547200b92a0d0d
Vincent Robert <vincent.robert@nanego.com>
2020-05-28 16:53:33 +0200

    Add utility methods in Organization model

--- lib/redmine_limited_visibility/models/organization_patch.rb
+++ lib/redmine_limited_visibility/models/organization_patch.rb
@@ -4,12 +4,32 @@ class Organization < ActiveRecord::Base
 
   has_many :organization_functions
 
+  def functions
+    Function.joins(:member_functions => {:member => :project})
+        .where("user_id IN (?) AND projects.status = ?", self.users.map(&:id), Project::STATUS_ACTIVE)
+        .sorted.uniq
+  end
+
   def functions_by_project(project)
-    Function.joins(:member_functions => :member).where("user_id IN (?) AND project_id = ?", self.users.map(&:id), project.id).uniq
+    Function.joins(:member_functions => :member)
+        .where("user_id IN (?) AND project_id = ?", self.users.map(&:id), project.id)
+        .sorted.uniq
+  end
+
+  def projects_by_function(function)
+    Project.joins(:members => :member_functions)
+        .where("function_id = ? AND user_id IN (?)", function.id, self.users.map(&:id))
+        .active.sorted.uniq
+  end
+
+  def users_with(project:, functions:)
+    users.joins(:members => :member_functions)
+        .where("users.status = ? AND members.project_id = ? AND function_id IN (?)", User::STATUS_ACTIVE, project.id, functions.map(&:id))
+        .sorted
   end
 
   def default_functions_by_project(project)
-    organization_functions.includes(:function).where("project_id = ?", project.id).map(&:function).reject{|f| f.blank?}.sort_by { |f| f.position}.uniq
+    organization_functions.includes(:function).where("project_id = ?", project.id).map(&:function).reject { |f| f.blank? }.sort_by { |f| f.position }.uniq
   end
 
   def delete_all_organization_functions(project_id, excluded_functions = [])

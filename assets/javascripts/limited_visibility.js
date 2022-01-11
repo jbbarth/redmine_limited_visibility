6cb9923f41aa987d606b64e2a0c87d346e603a1a
mathieu-mbru <m.brulin@adacis.net>
2022-01-11 10:56:49 +0100

    Envelopper les options cachées par balise< div style display none>

--- assets/javascripts/limited_visibility.js
+++ assets/javascripts/limited_visibility.js
@@ -84,12 +84,15 @@ function update_assigned_to_options() {
   $('#issue_assigned_to_id option').each(function(){
     var user_option = $(this);
     if(user_option.attr('functional_roles') && $('#involved-roles-form .role').length>0){
-      user_option.hide();
+      // Replace this line, Chrome does not support  option < style="display: none;">
+      // user_option.hide();
+      user_option.wrap('<div style="display: none;" />');
       $('#involved-roles-form .role').each(function() {
         var element = $(this);
         if (element.hasClass('involved')) {
           if (user_option.attr('functional_roles').split(',').indexOf(element.data('role-id').toString()) >= 0){
-            user_option.show();
+          //  user_option.show();
+            user_option.unwrap('div');
           }
         }
       });

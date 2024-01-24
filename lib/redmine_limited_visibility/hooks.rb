4e38eceb7c28d9a2f311a356b5272e6b0a89e70b
Vincent Robert <vincent.robert@nanego.com>
2024-01-24 10:42:16 +0100

    Patch core test-helper to cleanup patches loading

--- lib/redmine_limited_visibility/hooks.rb
+++ lib/redmine_limited_visibility/hooks.rb
@@ -7,7 +7,7 @@ module RedmineLimitedVisibility
     # Add our css/js on each page
     def view_layouts_base_html_head(context)
       stylesheet_link_tag("limited_visibility", plugin: "redmine_limited_visibility") +
-          stylesheet_link_tag("font-awesome.min.css", :plugin => "redmine_limited_visibility") +
+        stylesheet_link_tag("font-awesome.min.css", :plugin => "redmine_limited_visibility") +
         javascript_include_tag('limited_visibility.js', plugin: 'redmine_limited_visibility')
     end
 
@@ -16,11 +16,9 @@ module RedmineLimitedVisibility
   class ModelHook < Redmine::Hook::Listener
     def after_plugins_loaded(_context = {})
 
-      # unless Rails.env.test? #Avoid breaking core tests (specially csv core tests including ALL columns)
-        require_relative 'helpers/queries_helper_patch'
-        require_relative 'controllers/my_controller_patch'
-        require_relative 'models/issue_query_patch'
-      # end
+      require_relative 'helpers/queries_helper_patch'
+      require_relative 'controllers/my_controller_patch'
+      require_relative 'models/issue_query_patch'
 
       require_relative 'helpers/issues_helper_patch'
       require_relative 'helpers/issues_pdf_helper_patch'
@@ -42,6 +40,8 @@ module RedmineLimitedVisibility
         require_relative 'models/organization_patch'
         require_relative 'controllers/organizations_memberships_controller_patch'
       end
+
+      require_relative 'tests/test_helper_patch' if Rails.env.test?
 
     end
   end

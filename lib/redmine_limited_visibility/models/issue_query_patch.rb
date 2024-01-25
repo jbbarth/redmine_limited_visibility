6782e459884a747372202b50e09679cadb0e4ae0
Vincent Robert <vincent.robert@nanego.com>
2024-01-25 10:41:06 +0100

    Do not use deprecated methods

--- lib/redmine_limited_visibility/models/issue_query_patch.rb
+++ lib/redmine_limited_visibility/models/issue_query_patch.rb
@@ -236,7 +236,7 @@ class IssueQuery < Query
   # but ignore "can't be blank" error on authorized_viewers filter because it doesn't require any value
   def validate_query_filters
     super
-    m = label_for('authorized_viewers') + " " + l(:blank, scope: 'activerecord.errors.messages')
-    errors.messages[:base] = errors.messages[:base] - [m] if errors.messages[:base].present? && errors.messages[:base].include?(m)
+    m = "#{label_for('authorized_viewers')} #{l(:blank, scope: 'activerecord.errors.messages')}"
+    errors.delete(:base, m) if errors.messages[:base].present? && errors.messages[:base].include?(m)
   end
 end

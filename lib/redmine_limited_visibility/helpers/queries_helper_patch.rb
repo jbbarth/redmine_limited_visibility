d4a8ae0d120c6668acaaa769e0d6b4cec30d6d39
Vincent Robert <vincent.robert@nanego.com>
2024-01-25 10:57:45 +0100

    Update patch and reflect changes from Redmine 5.1.1

--- lib/redmine_limited_visibility/helpers/queries_helper_patch.rb
+++ lib/redmine_limited_visibility/helpers/queries_helper_patch.rb
@@ -89,53 +89,6 @@ module RedmineLimitedVisibility::Helpers
       end
     end
 
-    def filters_options_for_select(query)
-      ungrouped = []
-      grouped = {}
-      query.available_filters.map do |field, field_options|
-        if field_options[:type] == :relation
-          group = :label_relations
-        elsif field_options[:type] == :tree
-          group = query.is_a?(IssueQuery) ? :label_relations : nil
-        elsif field =~ /^cf_\d+\./
-          group = (field_options[:through] || field_options[:field]).try(:name)
-        elsif field =~ /^(.+)\./
-          # association filters
-          group = "field_#{$1}".to_sym
-
-          ## Start PATCH
-        elsif %w(member_of_group
-                assigned_to_role
-                assigned_to_member_with_function_id
-                assigned_to_function_id
-                has_been_assigned_to_id
-                has_been_assigned_to_function_id).include?(field)
-          group = :field_assigned_to
-        elsif %w(authorized_viewers has_been_visible_by_id).include?(field)
-          group = :field_authorized_viewers
-          ## End PATCH
-
-        elsif field_options[:type] == :date_past || field_options[:type] == :date
-          group = :label_date
-        end
-        if group
-          (grouped[group] ||= []) << [field_options[:name], field]
-        else
-          ungrouped << [field_options[:name], field]
-        end
-      end
-      # Don't group dates if there's only one (eg. time entries filters)
-      if grouped[:label_date].try(:size) == 1
-        ungrouped << grouped.delete(:label_date).first
-      end
-      s = options_for_select([[]] + ungrouped)
-      if grouped.present?
-        localized_grouped = grouped.map { |k, v| [k.is_a?(Symbol) ? l(k) : k.to_s, v] }
-        s << grouped_options_for_select(localized_grouped)
-      end
-      s
-    end
-
   end
 end
 
@@ -166,6 +119,61 @@ module QueriesHelper
     alias_method :retrieve_query_without_limited_visibility, :retrieve_query
     alias_method :retrieve_query, :retrieve_query_with_limited_visibility
     # we don't use prepend here because the helper is included in many controllers
+  end
+
+  def filters_options_for_select(query)
+    ungrouped = []
+    grouped = { label_string: [], label_date: [], label_time_tracking: [], label_attachment: [] }
+    query.available_filters.map do |field, field_options|
+      if /^cf_\d+\./.match?(field)
+        group = (field_options[:through] || field_options[:field]).try(:name)
+      elsif field =~ /^(.+)\./
+        # association filters
+        group = "field_#{$1}".to_sym
+
+        ## Start PATCH
+      elsif %w(member_of_group
+                assigned_to_role
+                assigned_to_member_with_function_id
+                assigned_to_function_id
+                has_been_assigned_to_id
+                has_been_assigned_to_function_id).include?(field)
+        group = :field_assigned_to
+      elsif %w(authorized_viewers has_been_visible_by_id).include?(field)
+        group = :field_authorized_viewers
+        ## End PATCH
+
+      elsif field_options[:type] == :relation
+        group = :label_relations
+      elsif field_options[:type] == :tree
+        group = query.is_a?(IssueQuery) ? :label_relations : nil
+      elsif field_options[:type] == :date_past || field_options[:type] == :date
+        group = :label_date
+      elsif %w(estimated_hours spent_time).include?(field)
+        group = :label_time_tracking
+      elsif %w(attachment attachment_description).include?(field)
+        group = :label_attachment
+      elsif [:string, :text, :search].include?(field_options[:type])
+        group = :label_string
+      end
+      if group
+        (grouped[group] ||= []) << [field_options[:name], field]
+      else
+        ungrouped << [field_options[:name], field]
+      end
+    end
+    # Remove empty groups
+    grouped.delete_if { |k, v| v.empty? }
+    # Don't group dates if there's only one (eg. time entries filters)
+    if grouped[:label_date].try(:size) == 1
+      ungrouped << grouped.delete(:label_date).first
+    end
+    s = options_for_select([[]] + ungrouped)
+    if grouped.present?
+      localized_grouped = grouped.map { |k, v| [k.is_a?(Symbol) ? l(k) : k.to_s, v] }
+      s << grouped_options_for_select(localized_grouped)
+    end
+    s
   end
 end
 

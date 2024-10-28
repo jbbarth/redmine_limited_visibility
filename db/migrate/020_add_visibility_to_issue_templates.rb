f3a157bd99daf5883ed88eaf8f327d13cf907f18
Vincent <contact@vincent-robert.com>
2024-10-28 10:16:25 +0100

    Avoid N+1 queries by preloading associations

--- db/migrate/020_add_visibility_to_issue_templates.rb
+++ db/migrate/020_add_visibility_to_issue_templates.rb
@@ -1,7 +1,11 @@
 class AddVisibilityToIssueTemplates < ActiveRecord::Migration[5.2]
   def change
-    if Redmine::Plugin.installed?(:redmine_templates)
-      add_column :issue_template_projects, :visibility, :string unless column_exists?(:issue_template_projects, :visibility)
+    if Redmine::Plugin.installed?(:redmine_templates) &&
+      table_exists?(:issue_template_projects) &&
+      !column_exists?(:issue_template_projects, :visibility)
+
+      add_column :issue_template_projects, :visibility, :string
+
     end
   end
 end
--- lib/redmine_limited_visibility/models/project_patch.rb
+++ lib/redmine_limited_visibility/models/project_patch.rb
@@ -57,12 +57,15 @@ class Project
 
   if Redmine::Plugin.installed?(:redmine_organizations)
     # Builds a nested hash of users sorted by function and organization
-    # => { Function(1) => { Org(1) => [ User(1), User(2), ... ] } }
+    # => { Function(1) => { Org(1) => [ User(1), User(2), ... ] } }
     #
     # TODO: simplify / refactor / test it correctly !!!
     def users_by_function_and_organization
       dummy_org = Organization.new(:name => l(:label_others))
-      self.members.map do |member|
+
+      members = self.members.includes(:roles, user: :organization)
+
+      members.map do |member|
         member.functions.sorted.map do |function|
           { :user => member.user, :function => function, :organization => member.user.organization }
         end

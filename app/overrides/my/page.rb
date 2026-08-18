509ebd5672f7c3d6f2023f8a1fe393586ad67dc6
Vincent <contact@vincent-robert.com>
2026-08-18 19:39:30 +0200

    Add plugin SVG sprite and use it for the admin menu icon

--- app/overrides/my/page.rb
+++ app/overrides/my/page.rb
@@ -1,10 +1,10 @@
 override =   %(
     <%= if params[:limited_visibility].present? && params[:limited_visibility] == 'false'
-          link_to l(:label_my_page_my_roles), my_page_path,
+          link_to sprite_icon('eye', l(:label_my_page_my_roles), plugin: 'redmine_limited_visibility'), my_page_path,
                 :class => "icon icon-visibility",
                 :id => "show_all_roles"
         else
-          link_to l(:label_my_page_all_roles), my_page_path(:limited_visibility => false),
+          link_to sprite_icon('eye', l(:label_my_page_all_roles), plugin: 'redmine_limited_visibility'), my_page_path(:limited_visibility => false),
                 :class => "icon icon-visibility",
                 :id => "show_all_roles"
         end %>
